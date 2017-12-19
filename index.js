28a07a2a6bfad1b36097b9eff53bd7c7e310e554
Felipe Francischetto <felipefrancischetto@hotmail.com>
2017-12-18 22:29:09 -0200

    exercicio-2

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express')
 const server = express()
 
-server.get('/', function(req, res) {
-    res.send('<h1>get</h1>')
-})
+server.route('/cliente')
+.get((req, res) => res.send('Lista de Clientes'))
+.post((req, res) => res.send('Novo Cliente'))
+.put((req, res) => res.send('Altera Cliente'))
+.delete((req, res) => res.send('Remove Cliente')) 
 
-server.post('/', function(req, res) {
-    res.send('<h1>post</h1>')
-})
-
-server.put('/', function(req, res) {
-    res.send('<h1>put</h1>')
-})
-
-server.delete('/', function(req, res) {
-    res.send('<h1>delete</h1>')
-})
-
-server.listen(3000, () => console.log('Running...'))
+server.listen(3000, () => console.log('Running ...'))
